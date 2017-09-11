f20d639d9caa74e5e2827b9f14de6ff065e7ebf0
justinturn <justinturn@gmail.com>
2017-09-11 08:32:57 -0500

    Update TwitterConnection.R

--- TwitterConnection.R
+++ TwitterConnection.R
@@ -3,11 +3,11 @@
 library("twitteR", "data.table")
 require(data.table)
   
-consumer_key <- "HuurwMMHUvuSNVXVp09vlaLvI"
-consumer_secret <- "LxHkoCNZyZaWk4fVc503wIxkgo6v6nQ35FuKhO1Ztzl8OTz10p"
-access_token <- "569538791-ysrvRNkkT2WF1JlKK5DBCqmTHLHGcQpzuEQ9bjSe"
-access_secret <- "q6HGoqizgZf82GADqA3OfuuOVBBMX2FYbsH1zyubfBC3y"
-options(httr_oauth_cache=T) #This will enable the use of a local file to cache OAuth access credentials between R sessions.
+consumer_key <- "ENTER HERE"
+consumer_secret <- "ENTER HERE"
+access_token <- "ENTER HERE"
+access_secret <- "ENTER HERE"
+options(httr_oauth_cache=T) #cache OAuth credentials between R sessions.
 setup_twitter_oauth(consumer_key,
                     consumer_secret,
                     access_token,
@@ -15,11 +15,11 @@ setup_twitter_oauth(consumer_key,
 ##END##
 
 ##Page Info##
-lifechurch <- getUser("lifechurch")
+lifechurch <- getUser("turnerjustinm")
 location(lifechurch)
 ##END PAGE INFO##
 
-lcfollowers<-lifechurch$getFollowers(retryOnRateLimit=200)
+lcfollowers<-lifechurch$getFollowers(retryOnRateLimit=200) #this could take a while here
 #print (length(lcfollowers))
 
 lc_followers_df = rbindlist(lapply(lcfollowers, as.data.frame))
@@ -28,4 +28,4 @@ lc_followers_matrix <- data.matrix(lc_followers_df, rownames.force=NA)
 
 head(lc_followers_df$location,10) 
 
-write.csv(lc_followers_df,"C:/Users/justin.turner/Desktop/twitterfollowers.csv")
+write.csv(lc_followers_df,"C:/Users/justin.turner/Desktop/twitterfollowers.csv") #here's your output dump. 
